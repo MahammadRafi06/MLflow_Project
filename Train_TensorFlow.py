5bc70bb1ad56ac2852a257b29d832796f461357a
Mahammad Rafi <mrafi@uw.edu>
2024-11-09 22:45:39 -0800

    Update Train_TensorFlow.py

--- Train_TensorFlow.py
+++ Train_TensorFlow.py
@@ -1,5 +1,7 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
+from sklearn.model_selection import train_test_split
 
 import tensorflow as tf
 from tensorflow.keras.layers import Input, Conv2D, Dense, Flatten, Dropout, GlobalMaxPooling2D
@@ -29,50 +31,52 @@ print(x_train.shape)
 mlflow.tensorflow.autolog()
 
 def mlflow_run(params, run_name="Tracking Experiment: TensorFlow - CNN "):
-  with mlflow.start_run(run_name=run_name) as run:
-    # get current run and experiment id
-    runID = run.info.run_uuid
-    experimentID = run.info.experiment_id
+    with mlflow.start_run(run_name=run_name) as run:  # Corrected 'r_name' to 'run_name'
+        # get current run and experiment id
+        runID = run.info.run_uuid
+        experimentID = run.info.experiment_id
+
+        # number of classes
+        K = len(set(y_train))
+        print("number of classes:", K)
         
-    # number of classes
-    K = len(set(y_train))
-    print("number of classes:", K)
-    # Build the model using the functional API
-    i = Input(shape=x_train[0].shape)
-    x = Conv2D(32, params['convSize'], strides=2, activation='relu')(i)
-    x = Conv2D(64, params['convSize'], strides=2, activation='relu')(x)
-    x = Conv2D(128, params['convSize'], strides=2, activation='relu')(x)
-    x = Flatten()(x)
-    x = Dropout(0.2)(x)
-    x = Dense(512, activation='relu')(x)
-    x = Dropout(0.2)(x)
-    x = Dense(K, activation='softmax')(x)
+        # Build the model using the functional API
+        i = Input(shape=x_train[0].shape)
+        x = Conv2D(32, params['convSize'], strides=2, activation='relu')(i)
+        x = Conv2D(64, params['convSize'], strides=2, activation='relu')(x)
+        x = Conv2D(128, params['convSize'], strides=2, activation='relu')(x)
+        x = Flatten()(x)
+        x = Dropout(0.2)(x)
+        x = Dense(512, activation='relu')(x)
+        x = Dropout(0.2)(x)
+        x = Dense(K, activation='softmax')(x)
 
-    model = Model(i, x)
+        model = Model(i, x)
 
-    # Compile and fit
-    # Note: make sure you are using the GPU for this!
-    model.compile(optimizer='adam',
-                  loss='sparse_categorical_crossentropy',
-                  metrics=['accuracy'])
-    r = model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=params['epochs'])
-    
-    # Plot accuracy per iteration
-    '''plt.plot(r.history['accuracy'], label='acc')
-    plt.plot(r.history['val_accuracy'], label='val_acc')
-    plt.legend()'''
-      
-  return (experimentID, runID)
+        # Compile and fit
+        # Note: make sure you are using the GPU for this!
+        model.compile(optimizer='adam',
+                      loss='sparse_categorical_crossentropy',
+                      metrics=['accuracy'])
+        r = model.fit(x_train, y_train, validation_data=(x_test, y_test), epochs=params['epochs'])
+
+        # Plot accuracy per iteration
+        plt.plot(r.history['accuracy'], label='acc')
+        plt.plot(r.history['val_accuracy'], label='val_acc')
+        plt.legend()
+
+    return (experimentID, runID)
 
 # Use the model
 if __name__ == '__main__':
-   # suppress any deprecated warnings
-   warnings.filterwarnings("ignore", category=DeprecationWarning)
+    # suppress any deprecated warnings
+    warnings.filterwarnings("ignore", category=DeprecationWarning)
 
-   convSize = int(sys.argv[1]) if len(sys.argv) > 1 else 3
-   epochs = int(sys.argv[2]) if len(sys.argv) > 2 else 10
-   params = {'epochs': epochs,
-            'convSize': (convSize,convSize)}
-   (exp_id, run_id) = mlflow_run(params)
+    convSize = int(sys.argv[1]) if len(sys.argv) > 1 else 3  # Default convSize to 3
+    epochs = int(sys.argv[2]) if len(sys.argv) > 2 else 3  # Fixed default epochs value to 3 (instead of (3, 3))
+    params = {'epochs': epochs,
+              'convSize': convSize}
+    
+    (exp_id, run_id) = mlflow_run(params)
 
-   print(f"Finished Experiment id={exp_id} and run id = {run_id}")
+    print(f"Finished Experiment id={exp_id} and run id = {run_id}")
